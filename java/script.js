015dc1c282ec81184bc67d0bca07f99172dd3b67
Brayden Wilder <braydenwilder@gmail.com>
2022-08-21 16:10:37 -0600

    java/script.js

--- java/script.js
+++ java/script.js
@@ -3,34 +3,73 @@
 // X Create a function for the player to make a choice 
 // X Write a function that compares choices 
 // X Return a string to prompt win or loss 
-// Make selection prompt case insensitive 
-// Write a function that allows for 5 round game 
+// X Make selection prompt case insensitive 
+// X Write a function that allows for 5 round game 
 
+let i = 1
 const choice = ['rock', 'paper', 'scissors'];
-let random = choice[Math.floor(Math.random() * choice.length)];
-let computerSelection = random;
-let playerSelection = prompt("Rock, Paper, or Scissors?").toLowerCase();
 
+while (i < 5) {
+    console.log(`Round ${i}`)
+    i++;
+    game();
+function game(){ 
+let playerGuess = prompt("Rock, Paper, or Scissors?");
+let playerSelection = playerGuess.toLowerCase();
 
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection != choice){
-        return console.log("Answer invalid, try again")
+    function answerCheck(){
+        if(choice.includes(playerSelection)) {
+            console.log(`You chose ${playerSelection}`)
+        } 
+           else{ 
+            return console.log("Answer invalid, try again.");
+            }
+        }
+    answerCheck();
+
+    getComputerChoice();
+    function getComputerChoice() {
+        const randNum = Math.floor(Math.random() * 3) + 1;
+            
+        switch(randNum) {
+            case 1:
+                computerSelection = "rock";
+                break;
+            case 2:
+                computerSelection = "paper";
+                break;    
+            case 3:
+                computerSelection = "scissors";
+                break;     
+        }
     }
-    else if(playerSelection == computerSelection){
-        return ("Draw!")
+
+    playRound(playerSelection, computerSelection); 
+    function playRound(playerSelection, computerSelection) {
+        if(playerSelection == computerSelection){
+            return console.log("Draw!");
+        }
+        else if(computerSelection == "rock"){
+            return console.log((playerSelection == "paper") ? "You Win!" : "You Lose!")
+        }
+        else if(computerSelection == "paper"){
+            return console.log((playerSelection == "scissors") ? "You Win!" : "You Lose!")
+        }
+        else if(computerSelection == "scissors"){
+            return console.log((playerSelection == "rock") ? "You Win!" : "You Lose!")
+        }
+    } 
     }
-    else if(computerSelection == "rock"){
-        return console.log((playerSelection == "paper") ? "You Win!" : "You Lose!")
-    }
-    else if(computerSelection == "paper"){
-        return console.log((playerSelection == "scissors") ? "You Win!" : "You Lose!")
-    }
-    else if(computerSelection == "scissors"){
-        return console.log((playerSelection == "rock") ? "You Win!" : "You Lose!")
-    }
+    if(i === 5) break;
 }
 
-playRound(playerSelection, computerSelection);
+    game();
+
+
+
+
+
+
 
 
 
